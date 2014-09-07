d5a3edabf0597bed4c23250762356a3eed88e77c
the-alex <thealex@umich.edu>
2014-09-07 06:30:22 -0400

    asdasd
    asdasda

---
+++ ios_application/Beaconizer/Beaconizer/iBeaconHandler.m
@@ -0,0 +1,13 @@
+//
+//  iBeaconHandler.m
+//  Beaconizer
+//
+//  Created by Alex Levine on 9/6/14.
+//  Copyright (c) 2014 Eliana and Alex and Alex and Alex. All rights reserved.
+//
+
+#import "iBeaconHandler.h"
+
+@implementation iBeaconHandler
+
+@end
